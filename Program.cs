707df28dcd5b124903c8bc6dbe460a4e7eb2f69a
Benjamin Bales <2591086+benjaminbales@users.noreply.github.com>
2020-12-17 14:36:03 +0000

    initial dotnet console project with hello world.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AxionBiosystemsChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AxionBiosystemsChallenge
 Creates a command line searchable database of employee data for small co. from a CSV.
+
+Using Microsoft.NETCore.App 5.0.0. 
+```dotnet new console```
