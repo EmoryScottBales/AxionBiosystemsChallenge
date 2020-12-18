6b530d31c246a7a9c03008a905ec9ecea8328b69
Benjamin Bales <2591086+benjaminbales@users.noreply.github.com>
2020-12-18 15:47:33 +0000

    implemented exception handling for command line args

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace AxionBiosystemsChallenge
     {
         static void Main(string[] args)
         {
-            //PopulateDB("Employees.csv");
+            //TODO: Refactor to add public PopulateDB(string csvfile) method to SmallCoEmployeeDb class.
             //Read CSV
             CsvFileReader reader = new CsvFileReader("Employees.csv");
             CsvRow row = new CsvRow();
@@ -25,7 +25,8 @@ namespace AxionBiosystemsChallenge
                     switch(i)
                     {
                         case 0:
-                            employee.Name = row[i];
+                            employee.FirstName = row[i].Substring(0, row[i].IndexOf(' '));
+                            employee.LastName = row[i].Substring(row[i].IndexOf(' ') + 1);
                             break;
                         case 1:
                             employee.ID = Int32.Parse(row[i]);
@@ -41,14 +42,15 @@ namespace AxionBiosystemsChallenge
                             break;
                     }
                 }
-                db.Add(new Employee { Name = employee.Name, ID = employee.ID, Birthday = employee.Birthday,
-                    Department = employee.Department, FavBook = employee.FavBook});
+                db.Add(new Employee { FirstName = employee.FirstName, LastName = employee.LastName, 
+                    ID = employee.ID, Birthday = employee.Birthday, Department = employee.Department, 
+                    FavBook = employee.FavBook});
             }
             db.SaveChanges();
 
+            /**********Handle Command Line Arguments************/
             for(int i = 0; i < args.Length; i++)
             {
-                // Console.WriteLine("args[{0}] is {1}", i, args[i]);
                 if(args[i].Equals("--h") || args[i].Equals("-help"))
                 {
                     Console.WriteLine("Enter --h or -help for the help menu.");
@@ -59,50 +61,54 @@ namespace AxionBiosystemsChallenge
                     break;
                 }else if(args[i].Equals("-name") || args[i].Equals("-Name"))
                 {
-                    //TODO: update model.cs and migrate db to separate name field into
-                    //first name and last name parsing on first white space.
+                    //last name parsing on an optional white space.
                     string pattern = @"[A-Z]+[ ]?[A-Z]+";
                     if((i < args.Length - 1) && 
                         Regex.Match(args[i+1], pattern, RegexOptions.IgnoreCase).Success)
                     {
-                        var e = db.Employees.Where(item => item.Name == args[i+1]).First();
-                        if(e != null)
+                        try
                         {
-                            Console.WriteLine("Name: {0}", e.Name);
+                            var e = db.Employees.Where(item => item.LastName == args[i+1]).First();
+                            Console.WriteLine("First Name: {0}", e.FirstName);
+                            Console.WriteLine("Last Name: {0}", e.LastName);
                             Console.WriteLine("ID: {0}", e.ID);
                             Console.WriteLine("Birthday: {0}", e.Birthday);
                             Console.WriteLine("Department: {0}", e.Department);
                             Console.WriteLine("Favorite Book: {0}", e.FavBook);
-                        }else
+                        }
+                        catch(InvalidOperationException ioe)
                         {
-                            Console.WriteLine("No matching database entry for Name = {0}.", args[i+1]);
+                            // Console.WriteLine(ioe.Message);
+                            Console.WriteLine("No matching database entry for Last Name = {0}.", args[i+1]);
                         }
                     }else
                     {
-                        Console.WriteLine("Invalid name.  Must be upper and lower case letters only.");
+                        Console.WriteLine("Invalid last name.  Must be upper and lower case letters only.");
                         break;
                     }
                 }else if(args[i].Equals("-ID") || args[i].Equals("-id"))
                 {
                     string pattern = @"\d+";
-                    if(Regex.Match(args[i+1], pattern).Success)
+                    if((i < args.Length - 1) && Regex.Match(args[i+1], pattern).Success)
                     {
-                        //need to implement try catch b/c if no match, the find will throw an exception that should be handled.
-                        var e = db.Employees.Where(item => item.ID == Int32.Parse(args[i+1])).First();
-                        if(e != null)
+                        try
                         {
-                            Console.WriteLine("Name: {0}", e.Name);
+                            var e = db.Employees.Where(item => item.ID == Int32.Parse(args[i+1])).First();
+                            Console.WriteLine("First Name: {0}", e.FirstName);
+                            Console.WriteLine("Last Name: {0}", e.LastName);
                             Console.WriteLine("ID: {0}", e.ID);
                             Console.WriteLine("Birthday: {0}", e.Birthday);
                             Console.WriteLine("Department: {0}", e.Department);
                             Console.WriteLine("Favorite Book: {0}", e.FavBook);
-                        }else
+                        }
+                        catch(InvalidOperationException ioe)
                         {
+                            // Console.WriteLine(ioe.Message);
                             Console.WriteLine("No matching database entry for ID = {0}.", args[i+1]);
                         }
                     }else
                     {
-                        Console.WriteLine("Invalid id.  Must be a positive integer.");
+                        Console.WriteLine("Invalid id.  Must be an integer.");
                         break;
                     }
                 }else
